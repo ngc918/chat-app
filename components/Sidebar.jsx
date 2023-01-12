0ce9b7fcc62479ede0b6fad7ee0f3e532076e20e
ngc.918 <nicolas.g9182@gmail.com>
2023-01-11 23:16:50 -0500

    ~

---
+++ components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/globals.css";
+
+const Sidebar = () => {
+	return <div className="w-[450px]"></div>;
+};
+
+export default Sidebar;
---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<div className="flex">
+			<Sidebar />
+			<Component {...pageProps} />
+		</div>
+	);
+}
+
+export default MyApp;
