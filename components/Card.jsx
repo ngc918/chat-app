2b045dde2a580aa6661d95ff2a7d6fa35dec9593
ngc.918 <nicolas.g9182@gmail.com>
2023-02-18 21:41:00 -0500

    ~

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import React from "react";
+import { collection, addDoc } from "firebase/firestore";
 import DefaultImage from "../public/images/default.png";
 
 const Card = ({ name, imageURL }) => {
+	const addChat = async () => {
+		const docRef = await addDoc(collection(db, "cities"), {
+			name: "Tokyo",
+			country: "Japan",
+		});
+	};
 	return (
 		<div className="w-full flex items-center py-3 px-5 border rounded-xl border-[#2b2b2b] space-x-6 cursor-pointer hover:bg-[#1d1d1d]">
 			<div className="rounded-full w-[60px] h-[60px] overflow-hidden">
@@ -15,7 +22,7 @@ const Card = ({ name, imageURL }) => {
 					quality={100}
 				/>
 			</div>
-			<span>{name}</span>
+			<span>{name.Card}</span>
 		</div>
 	);
 };
